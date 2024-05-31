c490e6eceb8693207a8bf347c2151fd197faed92
taurn kale <tkumarkale@gmail.com>
2024-05-31 15:28:03 +0530

    hello

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import endpoints from '../Services/movieServices'
 
 const Hero = () => {
+
+const [movie,setmovie]=useState({})
+
+useEffect(()=>{
+  axios.get(endpoints.popular).then((Response)=>{
+    console.log(Response)
+  })
+},[])
   return (
     <div>
       
